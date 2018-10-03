ccbc57ba18570d5681836b93bf11dccde1d7e953
mihaia <mihai.anghel@cegeka.com>
2018-10-03 17:48:57 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
+  /*templateUrl: `
+  <ul class='nav nav-pills'>
+          <li><a class='nav-link' [routerLink]="['/cities']">Cities</a></li>
+  </ul>
+  
+  <router-outlet></router-outlet>
+  `,*/
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CityInfo-UI';
+  title = 'City Info';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CityListComponent } from 'src/app/city/city-list.component';
+import {TableModule} from 'primeng/table';
+import { SidebarModule } from 'primeng/sidebar';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { MultiSelectModule } from 'primeng/multiselect';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {MenuItem} from 'primeng/api';
+import { MenuComponent } from './menu/Menu.component'; 
+import {HttpClientModule} from '@angular/common/http';
+import { PointOfInterestComponent } from './point-of-interest/point-of-interest.component';
+import {DataViewModule} from 'primeng/dataview';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityListComponent,
+    MenuComponent,
+    PointOfInterestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule,
+    TabMenuModule,
+    HttpClientModule,
+    DataViewModule,
+    RouterModule.forRoot([
+      {path: 'cities', component: CityListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
